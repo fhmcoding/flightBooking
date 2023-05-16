ee7f2b4fd258d0c1b57735cac6a29cb977c46bf4
fhmcoding <moutcho70@gmail.com>
2023-05-16 21:11:45 +0100

    ='fix'

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -8,7 +8,7 @@ const IndexPage = () => {
             <h2 className="text-4xl font-bold tracking-tight text-white sm:text-6xl">Support center</h2>
             <p className="my-6 text-lg leading-8 text-gray-300">
               Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet
-              fugiat veniam occaecat fugiat aliqua.
+              fugiat veniam occaecat fugiat aliqua
             </p>
             
             <SearchFlightForm />
